8a7ad079536505492f488a823c5bdb236b97fb2c
Mike Burrell <70190109+mikeburrell-stack@users.noreply.github.com>
2022-10-03 16:51:00 +0200

    Update client001.py

--- client001.py
+++ client001.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import pandas as pd
 
-st.write(""" # Forecast 2022 Oct
+# Markdown: # symbol refers to heading level 1, ## heading level 2
+st.write(""" # Forecast 2022 Oct """)
+
+st.write(""" Normal Text """)
+
+st.write(""" ## Model """)
+
+st.write(""" ### Data """)
+
+df = pd.DataFrame({
+  'col 1': [1,2,3],
+  'col 2': [10,20,30]
+})
+
+st.write(df)
 
 
-Whatever... """)
